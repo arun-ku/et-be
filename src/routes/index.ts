cff7f8998c5824c48351f07660a803d5403c9fbe
Arun Kumar <arun.kumar2@tbo.com>
2024-03-29 01:25:04 +0530

    [fix] - added user details route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import AuthRouter from "./Auth/router";
 import FamilyRouter from "./Family/router";
 import ExpenseCategoryRouter from "./ExpenseCategory/router";
+import UserRouter from "./User/router";
 import useAuth from "../middlewares/useAuth";
 import useFamily from "../middlewares/useFamily";
 
@@ -9,6 +10,7 @@ const routes = (app: Express) => {
   app.use("/api/auth", AuthRouter);
   app.use("/api/family", useAuth, FamilyRouter);
   app.use("/api/expense-category", useAuth, useFamily, ExpenseCategoryRouter);
+  app.use("/api/user", useAuth, UserRouter);
   app.get("/", (req, res) => {
     res.send("Hello World");
   });
