5f420b7e72965eefab579213cf48291516942c59
Arun Kumar <arun.kumar2@tbo.com>
2024-03-29 19:27:02 +0530

    [fix] - added expense apis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import AuthRouter from "./Auth/router";
 import FamilyRouter from "./Family/router";
 import ExpenseCategoryRouter from "./ExpenseCategory/router";
+import ExpenseRouter from "./Expense/router";
 import UserRouter from "./User/router";
 import useAuth from "../middlewares/useAuth";
 import useFamily from "../middlewares/useFamily";
@@ -10,6 +11,7 @@ const routes = (app: Express) => {
   app.use("/api/auth", AuthRouter);
   app.use("/api/family", useAuth, FamilyRouter);
   app.use("/api/expense-category", useAuth, useFamily, ExpenseCategoryRouter);
+  app.use("/api/expense", useAuth, useFamily, ExpenseRouter);
   app.use("/api/user", useAuth, UserRouter);
   app.get("/", (req, res) => {
     res.send("Hello World");
