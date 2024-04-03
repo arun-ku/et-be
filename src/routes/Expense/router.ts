304bfdea2bcbdf2c19a5f6233296c912a33228cd
Arun Kumar <arun.kumar2@tbo.com>
2024-04-03 23:15:32 +0530

    [feature] - added user and category wise expense list api

--- src/routes/Expense/router.ts
+++ src/routes/Expense/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import Expense from "../../models/Expense";
 import Users from "../../models/Users";
+import mongoose from "mongoose";
 
 const router = Router();
 
@@ -68,6 +69,162 @@ router.get("/listByUsers", async (req, res) => {
   ]);
 
   return res.sendResponse("200", expenses, true);
+});
+
+router.get("/listByUser/:id", async (req, res) => {
+  const family = req.family;
+  const expenses = await Expense.aggregate([
+    {
+      $facet: {
+        expenses: [
+          {
+            $match: {
+              familyId: family._id,
+              userId: new mongoose.Types.ObjectId(req.params.id),
+            },
+          },
+          { $sort: { createdAt: -1 } },
+          {
+            $lookup: {
+              from: "users",
+              localField: "userId",
+              foreignField: "_id",
+              as: "userId",
+              pipeline: [{ $project: { name: 1 } }],
+            },
+          },
+          {
+            $lookup: {
+              from: "expenseCategory",
+              localField: "categoryId",
+              foreignField: "_id",
+              as: "categoryId",
+              pipeline: [
+                {
+                  $project: {
+                    categoryName: 1,
+                    categoryIcon: 1,
+                    iconColor: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $project: {
+              amount: 1,
+              title: 1,
+              description: 1,
+              userId: { $first: "$userId" },
+              categoryId: { $first: "$categoryId" },
+              createdAt: 1,
+              familyId: 1,
+            },
+          },
+        ],
+        totalAmount: [
+          {
+            $match: {
+              familyId: family._id,
+              userId: new mongoose.Types.ObjectId(req.params.id),
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalAmount: { $sum: "$amount" },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        expenses: 1,
+        totalAmount: { $first: "$totalAmount.totalAmount" },
+      },
+    },
+  ]);
+
+  return res.sendResponse("200", expenses?.[0], true);
+});
+
+router.get("/listByCategory/:id", async (req, res) => {
+  const family = req.family;
+  const expenses = await Expense.aggregate([
+    {
+      $facet: {
+        expenses: [
+          {
+            $match: {
+              familyId: family._id,
+              categoryId: new mongoose.Types.ObjectId(req.params.id),
+            },
+          },
+          { $sort: { createdAt: -1 } },
+          {
+            $lookup: {
+              from: "users",
+              localField: "userId",
+              foreignField: "_id",
+              as: "userId",
+              pipeline: [{ $project: { name: 1 } }],
+            },
+          },
+          {
+            $lookup: {
+              from: "expenseCategory",
+              localField: "categoryId",
+              foreignField: "_id",
+              as: "categoryId",
+              pipeline: [
+                {
+                  $project: {
+                    categoryName: 1,
+                    categoryIcon: 1,
+                    iconColor: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $project: {
+              amount: 1,
+              title: 1,
+              description: 1,
+              userId: { $first: "$userId" },
+              categoryId: { $first: "$categoryId" },
+              createdAt: 1,
+              familyId: 1,
+            },
+          },
+        ],
+        totalAmount: [
+          {
+            $match: {
+              familyId: family._id,
+              categoryId: new mongoose.Types.ObjectId(req.params.id),
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalAmount: { $sum: "$amount" },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        expenses: 1,
+        totalAmount: { $first: "$totalAmount.totalAmount" },
+      },
+    },
+  ]);
+
+  return res.sendResponse("200", expenses?.[0], true);
 });
 
 router.get("/list", async (req, res) => {
